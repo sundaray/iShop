fcef45eda89912f231034e58863bf673f3b5ec08
sundaray <pgcim14.hemant@spjimr.org>
2022-12-26 14:19:02 +0530

    /api/products API endpoint created

---
+++ pages/api/products.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+export default async function handler(req, res) {
+
+    if (req.method === 'POST') {
+        try {
+            const {name, description, price, stockCount} = req.body;
+
+            const product = await prisma.product.create(
+                {
+                    data: {name, description, price, stockCount}
+                }
+            )
+            res.status(200).json(product);    
+        } catch (error) {
+            res.status(500).json({message: error.message})
+        }
+    }else {
+        res.setHeader('Allow', ['POST'])
+        res.status(405).json({
+            message:`HTTP method ${req.method} is not supported.`
+        })
+    }
+};
--- pages/products/product-upload.js
+++ pages/products/product-upload.js
@@ -8,7 +8,7 @@ import Spinner from "../../components/shared/Spinner";
 import FormInput from "../../components/shared/FormInput";
 import FormInputDesc from "../../components/shared/FormInputDesc";
 
-const Upload = () => {
+const ProductUpload = () => {
   const [success, setSuccess] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -49,7 +49,7 @@ const Upload = () => {
         onSubmit={formik.handleSubmit}
       >
         <div className="flex flex-col mx-8 my-8">
-          <h1 className="font-black text-3xl mb-6">Product</h1>
+          <h1 className="font-bold text-3xl mb-6">Product</h1>
           <FormInput formik={formik} label="Name" field="name" type="text" />
           <FormInputDesc formik={formik} />
           <FormInput
@@ -67,7 +67,7 @@ const Upload = () => {
           <button
             type="submit"
             disabled={loading}
-            className="shadow rounded w-36 bg-blue-600 text-white px-2 py-1 hover:bg-blue-700 hover:shadow-md disabled:opacity-50 disabled:cursor-not-allowed"
+            className="shadow rounded w-full bg-blue-600 text-blue-50 px-2 py-1 hover:bg-blue-700 hover:shadow-md disabled:opacity-50 disabled:cursor-not-allowed"
           >
             {loading ? <Spinner type="Uploading..." /> : "Upload"}
           </button>
@@ -76,4 +76,4 @@ const Upload = () => {
     </>
   );
 };
-export default Upload;
+export default ProductUpload;
