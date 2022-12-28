b8484cb113319e99c3a121b105889b5a0bf0261c
sundaray <pgcim14.hemant@spjimr.org>
2022-12-28 22:41:22 +0530

    login issue to be fixed

--- components/shared/navigation/Navigation.js
+++ components/shared/navigation/Navigation.js
@@ -1,22 +1,42 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../utils/firebase.config";
 
-const Navigation = ({ user, handleSignOut }) => {
+const Navigation = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser(localStorage.getItem("displayName") || null);
+  }, []);
+
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    localStorage.removeItem("displayName");
+    setUser(null);
+    signOut(auth).then(() => {
+      router.push("/");
+    });
+  };
+
   return (
     <>
       <nav className="w-full h-16 px-4 bg-gray-900 text-gray-400 flex items-center justify-between">
         <Link href="/">
           <h1 className="font-bold">iShop</h1>
         </Link>
-        <ul className="flex items-center space-x-6">
+        <ul className="flex items-center space-x-4">
           <Link href="/cart">
             <li>Cart</li>
           </Link>
-          {user ? (
-            <Link href="users/sign-in">
-              <li>Sign in</li>
-            </Link>
+          {!user ? (
+            ""
           ) : (
-            <li onClick={handleSignOut}>Sign out</li>
+            <li onClick={handleSignOut} className="cursor-pointer">
+              Sign out
+            </li>
           )}
         </ul>
       </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,10 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import "../styles/globals.css";
-import { signOut } from "firebase/auth";
-import { auth } from "../utils/firebase.config";
 import Navigation from "../components/shared/navigation/Navigation";
 
 export default function App({ Component, pageProps }) {
-  const [user, setUser] = useState(null);
-
-  const router = useRouter();
-
-  const handleSignOut = () => {
-    localStorage.removeItem("displayName");
-    setUser(null);
-    signOut(auth).then(() => {
-      router.push("/");
-    });
-  };
-
-  useEffect(() => {
-    setUser(localStorage.getItem("displayName") || null);
-  }, []);
-
   return (
     <>
-      <Navigation user={user} handleSignOut={handleSignOut} />
+      <Navigation />
       <Component {...pageProps} />
     </>
   );
