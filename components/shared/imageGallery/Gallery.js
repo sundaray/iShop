bcb47b756fbb9a592297a4693daa4fbe8d19291c
sundaray <pgcim14.hemant@spjimr.org>
2022-12-29 13:57:01 +0530

    swiper gallery ready

--- components/shared/imageGallery/Gallery.js
+++ components/shared/imageGallery/Gallery.js
@@ -1,41 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import React from "react";
+import { Navigation, Pagination, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Gallery = ({ imgUrls }) => {
-  const [index, setIndex] = useState(0);
-
-  console.log(imgUrls)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((i) => (i + 1) % imgUrls.length);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [imgUrls]);
-
-  const handlePrevClick = () => {
-    setIndex((i) => (i + imgUrls.length - 1) % imgUrls.length);
-  };
-
-  const handleNextClick = () => {
-    setIndex((i) => (i + 1) % imgUrls.length);
-  };
-
   return (
-    <div>
-      <AnimatePresence>
-        <motion.img
-          src={imgUrls[index]}
-          key={index}
-          initial={{ x: "100%" }}
-          animate={{ x: "0%" }}
-          exit={{ x: "-100%" }}
-          transition={{ duration: 1 }}
-        />
-      </AnimatePresence>
-      <button onClick={handlePrevClick}>Previous</button>
-      <button onClick={handleNextClick}>Next</button>
+    <div className="border w-1/3">
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {imgUrls.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${image}) center no-repeat`,
+                backgroundSize: "contain",
+              }}
+              className="w-full h-80"
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -2,33 +2,10 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { fetchProduct } from "../../utils/firebase.config";
-import { motion, AnimatePresence } from "framer-motion";
-import { wrap } from "popmotion";
-
-const variants = {
-  enter: (direction) => {
-    return {
-      x: direction > 0 ? 1000 : -1000,
-      opacity: 0,
-    };
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-  },
-  exit: (direction) => {
-    return {
-      zIndex: 0,
-      x: direction < 0 ? 1000 : -1000,
-      opacity: 0,
-    };
-  },
-};
+import Gallery from "../../components/shared/imageGallery/Gallery";
 
 const Product = () => {
   const [product, setProduct] = useState(null);
-  const [[page, direction], setPage] = useState([0, 0]);
 
   useEffect(() => {
     const productId = window.location.pathname.split("/")[2];
@@ -39,38 +16,9 @@ const Product = () => {
     return <p>Loading...</p>;
   }
 
-    const { imgUrls } = product;
-    const imageIndex = wrap(0, imgUrls.length, page);
-    const paginate = (newDirection) => {
-      setPage([page + newDirection, newDirection]);
-    };
-  
-
   return (
-    <>
-      <div className="example-container border">
-        <AnimatePresence initial={false} custom={direction}>
-          <motion.img
-            key={page}
-            src={product.imgUrls[imageIndex]}
-            custom={direction}
-            variants={variants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            transition={{
-              x: { type: "spring", stiffness: 300, damping: 30 },
-              opacity: { duration: 0.2 },
-            }}
-          />
-        </AnimatePresence>
-        <div className="next" onClick={() => paginate(1)}>
-          {"‣"}
-        </div>
-        <div className="prev" onClick={() => paginate(-1)}>
-          {"‣"}
-        </div>
-      </div>
+    <div className="mt-24">
+      <Gallery imgUrls={product.imgUrls} />
       <div>
         {product && (
           <div className="border rounded w-1/5 mt-24 px-4 py-4">
@@ -79,7 +27,7 @@ const Product = () => {
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
