b7239a502afab3e7131fe47fa428cbbc95d74b4f
sundaray <pgcim14.hemant@spjimr.org>
2023-01-02 18:38:03 +0530

    product description page text size changed

--- components/shared/product/ProductDescription.js
+++ components/shared/product/ProductDescription.js
@@ -1,7 +1,7 @@
 const ProductDescription = ({ product: { description } }) => {
   return (
     <div className="product-description rounded-xl bg-gray-100 px-5 py-3 md:px-10 md:py-4">
-      <p className="text-sm md:text-xl font-medium">{description}</p>
+      <p className="text-sm md:text-lg font-medium">{description}</p>
     </div>
   );
 };
