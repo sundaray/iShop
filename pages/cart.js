4de292ad1cfca64251d8c35c397c702dcd74f5dc
sundaray <pgcim14.hemant@spjimr.org>
2023-01-02 19:44:40 +0530

    cart page responsiveness is complete

--- pages/cart.js
+++ pages/cart.js
@@ -43,22 +43,22 @@ const Cart = () => {
       {loading ? (
         <PageSpinner />
       ) : (
-        <main className="w-9/12 mt-32 mb-12 m-auto">
+        <main className="w-full lg:w-9/12 mt-32 mb-12 m-auto">
           <div className="flex flex-col items-center space-y-8 mb-12 ">
-            <p className="text-4xl font-medium text-gray-900">
+            <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-900">
               {cartItemsQty > 0
                 ? `Your cart total is $${totalPrice}.`
-                : "Your cart is empty."}
+                : "Your cart is empty"}
             </p>
             <Link href={cartItemsQty > 0 ? "/checkout" : "/"}>
               <button
-                className={`shadow rounded w-auto py-2 ${
+                className={`shadow rounded w-42 py-2 ${
                   cartItemsQty > 0
                     ? "bg-blue-600 text-blue-50 hover:bg-blue-700"
                     : "bg-gray-100 hover:bg-gray-200"
                 } px-2  hover:shadow-md disabled:opacity-50 disabled:cursor-not-allowed transition-all`}
               >
-                {cartItemsQty > 0 ? "Check Out" : "Continue Shopping"}
+                {cartItemsQty > 0 ? "Check out" : "Continue shopping"}
               </button>
             </Link>
           </div>
@@ -69,13 +69,13 @@ const Cart = () => {
               cartItems.map(({ name, id, price, qty, stockCount, imgUrls }) => (
                 <div
                   key={id}
-                  className="w-3/5 mb-8 px-8 py-4 bg-gray-100 rounded-md flex justify-between items-center m-auto"
+                  className="w-full md:w-4/5 lg:w-3/5 mb-8 px-4 lg:px-8 py-2 lg:py-4 bg-gray-100 rounded-xl flex justify-between items-center m-auto"
                 >
                   <Image
                     priority
                     src={imgUrls[0]}
-                    width={150}
-                    height={150}
+                    width={100}
+                    height={100}
                     alt={name}
                   />
                   <select
