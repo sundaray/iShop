308470813195560304dccb087b99854dce5758a8
sundaray <pgcim14.hemant@spjimr.org>
2023-01-03 23:11:51 +0530

    Added /customer API route

--- components/stripe/success.js
+++ components/stripe/success.js
@@ -1,12 +1,34 @@
-import React from "react";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Check } from "@heroicons/react/24/solid";
 
 const Success = () => {
-  const {
-    query: { session_id },
-  } = useRouter();
+  const [customerName, setCustomerName] = useState(null);
 
-  return <h1>{session_id}</h1>;
+  useEffect(() => {
+    const fetchCustomerName = async () => {
+      const { name } = await axios.get("/api/customer");
+      if (name) {
+        setCustomerName(name);
+      }
+    };
+    fetchCustomerName();
+  }, []);
+
+  return (
+    <>
+      {customerName ? (
+        <div className="flex flex-col items-center">
+          <h1>
+            <Check />
+            Thanks for the order {customerName}!
+          </h1>
+        </div>
+      ) : (
+        <h1>Sorry, it looks like the payment did not go through.</h1>
+      )}
+    </>
+  );
 };
 
 export default Success;
---
+++ pages/api/customer.js
@@ -0,0 +1,21 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+
+    const {session_id} = req.query;
+    
+  if (req.method === "GET") {
+    try {
+      const { name } = await stripe.customers.retrieve(session_id);
+
+      res.status(200).json({ name });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  } else {
+    res.setHeader("Allow", "GET");
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
