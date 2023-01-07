0c8012529c0ace94f12dd3136186604cf7328d99
sundaray <pgcim14.hemant@spjimr.org>
2023-01-07 22:04:21 +0530

    finished the rating component

--- components/shared/product/ProductRating.js
+++ components/shared/product/ProductRating.js
@@ -8,42 +8,63 @@ const ProductRating = ({ rating, noOfReviews }) => {
   const ratingNumber = Number(rating);
 
   return (
-    <div>
-      {ratingNumber >= 1 ? (
-        <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
-      ) : ratingNumber >= 0.5 ? (
-        <FontAwesomeIcon icon={faStarHalfStroke} className="text-yellow-400" />
-      ) : (
-        <StarIcon className="inline w-5 h-5 text-yellow-400" />
-      )}
-      {ratingNumber >= 2 ? (
-        <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
-      ) : ratingNumber >= 1.5 ? (
-        <FontAwesomeIcon icon={faStarHalfStroke} className="text-yellow-400" />
-      ) : (
-        <StarIcon className="inline w-5 h-5 text-yellow-400" />
-      )}
-      {ratingNumber >= 3 ? (
-        <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
-      ) : ratingNumber >= 2.5 ? (
-        <FontAwesomeIcon icon={faStarHalfStroke} className="text-yellow-400" />
-      ) : (
-        <StarIcon className="inline w-5 h-5 text-yellow-400" />
-      )}
-      {ratingNumber >= 4 ? (
-        <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
-      ) : ratingNumber >= 3.5 ? (
-        <FontAwesomeIcon icon={faStarHalfStroke} className="text-yellow-400" />
-      ) : (
-        <StarIcon className="inline w-5 h-5 text-yellow-400" />
-      )}
-      {ratingNumber >= 5 ? (
-        <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
-      ) : ratingNumber >= 4.5 ? (
-        <FontAwesomeIcon icon={faStarHalfStroke} className="text-yellow-400" />
-      ) : (
-        <StarIcon className="inline w-5 h-5 text-yellow-400" />
-      )}
+    <div className="flex items-center">
+      <div className="mr-2 mb-2">
+        {ratingNumber >= 1 ? (
+          <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+        ) : ratingNumber >= 0.5 ? (
+          <FontAwesomeIcon
+            icon={faStarHalfStroke}
+            className="text-yellow-400"
+          />
+        ) : (
+          <StarIcon className="inline w-5 h-5 text-yellow-400" />
+        )}
+        {ratingNumber >= 2 ? (
+          <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+        ) : ratingNumber >= 1.5 ? (
+          <FontAwesomeIcon
+            icon={faStarHalfStroke}
+            className="text-yellow-400"
+          />
+        ) : (
+          <StarIcon className="inline w-5 h-5 text-yellow-400" />
+        )}
+        {ratingNumber >= 3 ? (
+          <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+        ) : ratingNumber >= 2.5 ? (
+          <FontAwesomeIcon
+            icon={faStarHalfStroke}
+            className="text-yellow-400"
+          />
+        ) : (
+          <StarIcon className="inline w-5 h-5 text-yellow-400" />
+        )}
+        {ratingNumber >= 4 ? (
+          <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+        ) : ratingNumber >= 3.5 ? (
+          <FontAwesomeIcon
+            icon={faStarHalfStroke}
+            className="text-yellow-400"
+          />
+        ) : (
+          <StarIcon className="inline w-5 h-5 text-yellow-400" />
+        )}
+        {ratingNumber >= 5 ? (
+          <FontAwesomeIcon icon={faStar} className="text-yellow-400" />
+        ) : ratingNumber >= 4.5 ? (
+          <FontAwesomeIcon
+            icon={faStarHalfStroke}
+            className="text-yellow-400"
+          />
+        ) : (
+          <StarIcon className="inline w-5 h-5 text-yellow-400" />
+        )}
+      </div>
+      <div>
+      <span className="mr-1">{noOfReviews}</span>
+      <span>{`${noOfReviews > 1 ? "reviews" : "review"}`}</span>
+      </div>
     </div>
   );
 };
